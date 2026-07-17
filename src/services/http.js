0acc1e8fb36f78656288c1333d7c866f4ac72c2c
Vũ Trung Tiến <22dh113737@st.huflit.edu.vn>
2026-07-18 02:08:41 +0700

    edit: http.js

--- src/services/http.js
+++ src/services/http.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.DEV
+  ? import.meta.env.VITE_API_URL_DEV
+  : import.meta.env.VITE_API_URL_PROD;
+
 const http = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  baseURL: baseURL || 'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
